c034520ac43e1e484a93cfaab0a02a6e2bd9db7a
Paco Moreno <pakerimus@gmail.com>
2018-09-28 12:01:43 -0300

    se completa formulario referentes, sin save

--- src/renderer/components/Referent/EditReferent.vue
+++ src/renderer/components/Referent/EditReferent.vue
@@ -1,44 +1,88 @@
 <template>
-  <div style="padding: 20px">
+  <div id="referents-data">
     <div class="page-header">
       <span>Editar Referente</span>
       <router-link :to="{ name: 'referent', params: { id: $route.params.id } }">
-        <el-button type="warning">Cancelar</el-button>
+        <el-button type="info">Volver</el-button>
       </router-link>
     </div>
-    <h3>{{ $route.params.id }}</h3>
-    <el-form
-      ref="form"
-      :model="referent"
-      label-width="120px">
-      <el-form-item label="Nombres">
-        <el-input v-model="referent.firstname"/>
-      </el-form-item>
-      <el-form-item label="Apellidos">
-        <el-input v-model="referent.lastname"/>
-      </el-form-item>
-      <el-form-item label="Rubros">
-        <el-select
-          v-model="referent.areas"
-          multiple
-          style="width: 100%"
-          placeholder="Select">
-          <el-option
-            v-for="item in areaOptions"
-            :key="item"
-            :label="item"
-            :value="item"/>
-        </el-select>
-      </el-form-item>
-      <el-form-item>
-        <!--
-        <el-button
-          type="primary"
-          @click="onSubmit">Guardar</el-button>
-        <el-button>Cancelar</el-button>
-        -->
-      </el-form-item>
-    </el-form>
+    <div
+      v-if="referentId"
+      class="data">
+      <el-form
+        ref="form"
+        :model="referent"
+        label-position="top" 
+        label-width="120px">
+        <el-form-item label="Nombres">
+          <el-input v-model="referent.firstname"/>
+        </el-form-item>
+        <el-form-item label="Apellidos">
+          <el-input v-model="referent.lastname"/>
+        </el-form-item>
+        <el-form-item label="Telefono">
+          <el-input v-model="referent.phone"/>
+        </el-form-item>
+        <el-form-item label="Celular">
+          <el-input v-model="referent.cellphone"/>
+        </el-form-item>
+        <el-form-item label="Email">
+          <el-input v-model="referent.email"/>
+        </el-form-item>
+        <el-form-item label="Direccion">
+          <el-input v-model="referent.address"/>
+        </el-form-item>
+        <el-form-item label="Cargo">
+          <el-select
+            v-model="referent.position"
+            filterable
+            style="width: 100%"
+            placeholder="Select">
+            <el-option
+              v-for="item in positionOptions"
+              :key="item"
+              :label="item"
+              :value="item"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Rubros">
+          <el-select
+            v-model="referent.areas"
+            multiple
+            filterable
+            style="width: 100%"
+            placeholder="Select">
+            <el-option
+              v-for="item in areaOptions"
+              :key="item"
+              :label="item"
+              :value="item"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="Zonas">
+          <el-select
+            v-model="referent.zones"
+            multiple
+            filterable
+            style="width: 100%"
+            placeholder="Select">
+            <el-option
+              v-for="item in zoneOptions"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            type="success"
+            @click="onSubmit">Guardar</el-button>
+          <el-button
+            type="danger"
+            @click="onReset">Reset</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -48,6 +92,7 @@ const { mapGetters: referentGetters, mapActions: referentActions } = namespace(
   "referent"
 );
 const { mapActions: listValuesActions } = namespace("admin");
+const { mapActions: zoneActions } = namespace("zone");
 
 export default {
   name: "EditReferent",
@@ -55,21 +100,28 @@ export default {
     return {
       loading: false,
       referent: {},
-      areaOptions: []
+      referentId: null,
+      areaOptions: [],
+      positionOptions: [],
+      zoneOptions: []
     };
   },
   created() {
     this.loadReferent();
     this.loadReferentAreas();
+    this.loadReferentPositions();
+    this.loadZones();
   },
   methods: {
     ...referentActions(["fetchReferent"]),
     ...listValuesActions(["fetchListValues"]),
+    ...zoneActions(["fetchZones"]),
     loadReferent() {
       this.loading = true;
       this.fetchReferent({ referentId: this.$route.params.id })
         .then(response => {
           this.referent = response.data.attributes;
+          this.referentId = response.data.id;
         })
         .catch(error => {
           console.log(error);
@@ -90,7 +142,50 @@ export default {
         .finally(() => {
           this.loading = false;
         });
+    },
+    loadReferentPositions() {
+      this.loading = true;
+      this.fetchListValues({ list: "referent_positions" })
+        .then(response => {
+          this.positionOptions = response;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    loadZones() {
+      this.loading = true;
+      this.fetchZones({ criteria: "" })
+        .then(response => {
+          this.zoneOptions = response;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    onSubmit() {
+      console.log("submit referent", this.referent);
+    },
+    onReset() {
+      this.loadReferent();
     }
   }
 };
 </script>
+
+<style>
+#referents-data {
+  background: #fff;
+  height: 100%;
+}
+
+.data {
+  padding: 0px 20px;
+}
+</style>
